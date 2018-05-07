f21265f1ffcc0c84a5496222cbd4e008cac0f929
BJR Matos <bjrmatos@gmail.com>
2018-05-06 19:18:19 -0500

    add optionsSchema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,32 @@
+
 module.exports = {
   'name': 'html-to-text',
+  'main': 'lib/main.js',
   'dependencies': ['templates'],
-  'main': 'lib/main.js'
+  'optionsSchema': {
+    extensions: {
+      'html-to-text': {
+        type: 'object',
+        properties: {
+          wordWrap: { type: 'number' },
+          hideLinkHrefIfSameAsText: { type: 'boolean' },
+          ignoreHref: { type: 'boolean' },
+          ignoreImage: { type: 'boolean' },
+          preserveNewlines: { type: 'boolean' },
+          uppercaseHeadings: { type: 'boolean' },
+          singleNewLineParagraphs: { type: 'boolean' },
+          returnDomByDefault: { type: 'boolean' },
+          forceWrapOnLimit: { type: 'boolean' },
+          longWordSplit: {
+            type: 'object',
+            properties: {
+              wrapCharacters: { type: 'string' },
+              forceWrapOnLimit: { type: 'boolean' }
+            }
+          },
+          longWordSplitForceWrapOnLimit: { type: 'boolean' }
+        }
+      }
+    }
+  }
 }
